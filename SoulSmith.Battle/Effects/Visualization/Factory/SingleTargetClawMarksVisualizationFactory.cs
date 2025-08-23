8f00025bd88593bb6d59fbb41538e0bb981d844d
BikeDouble <elc12@sfu.ca>
2025-08-23 00:40:40 -0700

    Added SingleTargetClawMarks effect visualization

---
+++ SoulSmith.Battle/Effects/Visualization/Factory/SingleTargetClawMarksVisualizationFactory.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.Xna.Framework;
+using SoulSmith.Drawing;
+using SoulSmith.Vector;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SoulSmith.Battle.Effects.Visualization.Factory
+{
+    [JsonConverter(typeof(SingleTargetClawMarksVisualizationFactoryJsonConverter))]
+    public class SingleTargetClawMarksVisualizationFactory : EffectVisualizationFactory
+    {
+        private Vector2 _size;
+        private Color _color;
+        private string _particleResourceKey;
+        private float _slashTime;
+        private float _lingerTime;
+        private float _fadeTime;
+
+        public SingleTargetClawMarksVisualizationFactory(string particleResourceKey,
+            Vector2 size,
+            Color color,
+            float slashTime,
+            float lingerTime,
+            float fadeTime,
+            float delay = 0f) : base(slashTime + lingerTime + fadeTime, slashTime / 2, delay)
+        {
+            _particleResourceKey = particleResourceKey;
+            _size = size;
+            _color = color;
+            _slashTime = slashTime;
+            _lingerTime = lingerTime;
+            _fadeTime = fadeTime;
+        }
+
+        public override EffectVisualization CreateVisualization()
+        {
+            return new SingleTargetClawMarksVisualization(_particleResourceKey, _size, _color, _slashTime, _lingerTime, _fadeTime, Lifespan, EffectActivationTimer, Delay);
+        }
+    }
+
+    public class SingleTargetClawMarksVisualizationFactoryJsonConverter : JsonConverter<SingleTargetClawMarksVisualizationFactory>
+    {
+        public override SingleTargetClawMarksVisualizationFactory Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType != JsonTokenType.StartObject) throw new JsonException("Expected start of an object");
+
+            reader.Read();
+
+            float slashTime = 0f;
+            float lingerTime = 0f;
+            float fadeTime = 0f;
+            float delay = 0f;
+            string particleResourceKey = null;
+            Vector2? size = null;
+            Color color = Color.White;
+
+            while (reader.TokenType != JsonTokenType.EndObject)
+            {
+                if (reader.TokenType != JsonTokenType.PropertyName) throw new JsonException("Expected property name");
+
+                string propertyName = reader.GetString();
+
+                reader.Read();
+
+                switch (propertyName)
+                {
+                    case "Delay":
+                        if (reader.TokenType != JsonTokenType.Number) throw new JsonException("Expected number");
+                        delay = (float)reader.GetDouble();
+                        reader.Read();
+                        break;
+                    case "SlashTime":
+                        if (reader.TokenType != JsonTokenType.Number) throw new JsonException("Expected number");
+                        slashTime = (float)reader.GetDouble();
+                        reader.Read();
+                        break;
+                    case "LingerTime":
+                        if (reader.TokenType != JsonTokenType.Number) throw new JsonException("Expected number");
+                        lingerTime = (float)reader.GetDouble();
+                        reader.Read();
+                        break;
+                    case "FadeTime":
+                        if (reader.TokenType != JsonTokenType.Number) throw new JsonException("Expected number");
+                        fadeTime = (float)reader.GetDouble();
+                        reader.Read();
+                        break;
+                    case "ParticleSprite":
+                    case "Sprite":
+                        if (reader.TokenType != JsonTokenType.String) throw new JsonException("Expected string");
+                        particleResourceKey = reader.GetString();
+                        reader.Read();
+                        break;
+                    case "Size":
+                        if (reader.TokenType != JsonTokenType.StartObject) throw new JsonException("Expected start of object");
+                        Vector2JsonConverter converter = new Vector2JsonConverter();
+                        size = converter.Read(ref reader, typeof(Vector2), options);
+                        reader.Read();
+                        break;
+                    case "Color":
+                        if (reader.TokenType != JsonTokenType.StartArray) throw new JsonException("Expected start of array");
+                        ColorJsonConverter colorConverter = new ColorJsonConverter();
+                        color = colorConverter.Read(ref reader, typeof(Color), options);
+                        reader.Read();
+                        break;
+                    default:
+                        reader.Skip();
+                        break;
+                }
+            }
+
+            if (particleResourceKey == null) throw new JsonException("ParticleSprite is required");
+            if (size == null) throw new JsonException("Size is required");
+            if (slashTime + fadeTime + lingerTime <= 0) throw new JsonException("Lifespan must be greater than 0");
+
+            return new SingleTargetClawMarksVisualizationFactory(particleResourceKey, size.Value, color, slashTime, lingerTime, fadeTime, delay);
+        }
+
+        public override void Write(Utf8JsonWriter writer, SingleTargetClawMarksVisualizationFactory value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SoulSmith.Battle/Effects/Visualization/SingleTargetClawMarksVisualization.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Xna.Framework;
+using SoulSmith.Asset;
+using SoulSmith.Core;
+using SoulSmith.Drawing;
+using SoulSmith.Object;
+using SoulSmith.Object.Canvas;
+using System;
+
+namespace SoulSmith.Battle.Effects.Visualization
+{
+    public class SingleTargetClawMarksVisualization : EffectVisualization
+    {
+        private Vector2 _size;
+        private Color _color;
+        private float _slashTime;
+        private float _lingerTime;
+        private float _fadeTime;
+
+        // Children
+        private ScissorRect _particleScissorRect;
+
+        public SingleTargetClawMarksVisualization(string particleResourceKey,
+            Vector2 size,
+            Color color,
+            float slashTime,
+            float lingerTime,
+            float fadeTime,
+            float lifespan,
+            float effectActivationTimer,
+            float delay = 0f) : base(lifespan, effectActivationTimer, delay)
+        {
+            _size = size;
+            _color = color;
+            _slashTime = slashTime;
+            _lingerTime = lingerTime;
+            _fadeTime = fadeTime;
+
+            IDrawableResource particleResource = DrawHelpers.GetDrawableResourceInstance(particleResourceKey);
+
+            CanvasObject particle = new CanvasObject(new Position(_size / 2), particleResource);
+            particle.ScaleToSetSize(size);
+
+            _particleScissorRect = new ScissorRect(null, (int)_size.X, (int)0, null, new List<SoulSmithObject>{ particle });
+            _particleScissorRect.SetOriginPlacement(OriginPlacement.TopLeft);
+            this.SetColor(color);
+
+            AddChild(_particleScissorRect);
+        }
+
+        public override void BeginVisualization(IReadOnlyUnit sender, IReadOnlyUnit target, float delay = 0)
+        {
+            base.BeginVisualization(sender, target, delay);
+
+            Vector2 startPoint;
+
+            IReadOnlyUnitSprite targetSprite = target.ReadOnlySprite;
+
+            startPoint = Target.GetGlobalPosition().Coordinates;
+
+            _particleScissorRect.SetCoordinates(startPoint - (_size / 2));
+        }
+
+        protected override void EnabledProcess(double delta)
+        {
+            base.EnabledProcess(delta);
+
+            float slashProgress = Math.Clamp((ElapsedLifespan / _slashTime), 0f, 1f);
+            float fadeProgress = Math.Clamp(((ElapsedLifespan - _slashTime - _lingerTime) / _fadeTime), 0f, 1f);
+
+            // Slash
+            int currentY = (int)(_size.Y * slashProgress);
+
+            // Fade
+            int currentAlpha = 255 - (int)(255 * fadeProgress);
+
+            _particleScissorRect.SetColor(255, 255, 255, currentAlpha);
+
+            _particleScissorRect.Height = currentY;
+        }
+    }
+}
